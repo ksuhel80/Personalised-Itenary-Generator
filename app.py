2474ff5043c723cbddfc5199bfd146229da6681f
Khan Suhel Ahmed <ksuhel80@gmail.com>
2025-02-07 19:16:21 +0530

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import pandas as pd 
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import joblib
